e7f869bfa52c4a594f51eeee7c52e4c5e952d9c3
斎藤正大 <saima.ptw88@gmail.com>
2021-12-19 22:19:20 +0900

    add : components

---
+++ components/ReccomendedBook.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="reccomended-book">
+    <h1>qiita article title</h1>
+    <p>ranking number</p>
+    <div>tags</div>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style>
+.reccomended-book {
+  height: 50px;
+}
+</style>
