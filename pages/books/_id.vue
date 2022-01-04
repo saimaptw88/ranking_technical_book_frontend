3d3ff4518b5adfe55fb8700729075dfe87c837a7
斎藤正大 <saima.ptw88@gmail.com>
2022-01-04 11:53:26 +0900

    add : new page books/_id.vue

---
+++ pages/books/_id.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div>title : {{ title }}</div>
+    <div>authors : {{ authors }}</div>
+    <div>publicationData : {{ publicationData }}</div>
+    <div>explanation : {{ explanation }}</div>
+    <img :src="thumbnailUrl" />
+    <div>affiliateUrl : {{ affiliateUrl }}</div>
+    <div>qiitaLgtm : {{ qiitaLgtm }}</div>
+    <div>qiitaTitles : {{ qiitaTitles }}</div>
+    <div>qiitaTags : {{ qiitaTags }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator"
+import { Context } from '@nuxt/types'
+
+@Component({
+  async asyncData(context: Context) {
+    const { store, route } = context
+
+    const bookId = route.params.id  // dose get data?
+    await store.dispatch("reccomendedBook/setReccomendedBook", bookId)
+
+    const title = store.getters['reccomendedBook/title']
+    const authors = store.getters['reccomendedBook/authors']
+    const publicationData = store.getters['reccomendedBook/publicationData']
+    const explanation = store.getters['reccomendedBook/explanation']
+    const thumbnailUrl = store.getters['reccomendedBook/thumbnailUrl']
+    const affiliateUrl = store.getters['reccomendedBook/affiliateUrl']
+    const qiitaLgtm = store.getters['reccomendedBook/qiitaLgtm']
+    const qiitaTitles = store.getters['reccomendedBook/qiitaTitles']
+    const qiitaTags = store.getters['reccomendedBook/qiitaTags']
+
+    return{
+      title: title,
+      authors: authors,
+      publicationData: publicationData,
+      explanation: explanation,
+      thumbnailUrl: thumbnailUrl,
+      affiliateUrl: affiliateUrl,
+      qiitaLgtm: qiitaLgtm,
+      qiitaTitles: qiitaTitles,
+      qiitaTags: qiitaTags
+    }
+  }
+})
+export default class Id extends Vue{
+  // vue getters
+  get title(){
+    return this.$store.getters['reccomendedBook/title']
+  }
+  get authors(){
+    return this.$store.getters['reccomendedBook/authors']
+  }
+  get publicationData(){
+    return this.$store.getters['reccomendedBook/publicationData']
+  }
+  get explanation(){
+    return this.$store.getters['reccomendedBook/explanation']
+  }
+  get thumbnailUrl(){
+    return this.$store.getters['reccomendedBook/thumbnailUrl']
+  }
+  get affiliateUrl(){
+    return this.$store.getters['reccomendedBook/affiliateUrl']
+  }
+  get qiitaLgtm(){
+    return this.$store.getters['reccomendedBook/qiitaLgtm']
+  }
+  get qiitaTitles(){
+    return this.$store.getters['reccomendedBook/qiitaTitles']
+  }
+  get qiitaTags(){
+    return this.$store.getters['reccomendedBook/qiitaTags']
+  }
+}
+</script>
